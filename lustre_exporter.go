8128c8c2a60fd96a7c568702126b3878382a726e
Joe Handzik <joseph.t.handzik@hpe.com>
2017-01-04 15:20:16 -0600

    lustre_exporter.go: Initial commit
    Basic infrastructure and flag definitions. Some assumptions made about what the lustre.go code will look like.
    
    Signed-Off-By: Joe Handzik <joseph.t.handzik@hpe.com>

--- lustre_exporter.go
+++ lustre_exporter.go
@@ -10,3 +10,62 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	
+	"github.com/joehandzik/lustre_exporter/exporter"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/log"
+)
+
+var (
+	showVersion	= flag.Bool("version", false, "Print version information.")
+	listenAddress	= flag.String("web.listen-address", ":9119", "Address to use to expose Lustre metrics.")
+	metricsPath	= flag.String("web.telemetry-path", "/metrics", "Path to use to expose Lustre metrics.")
+)
+
+func init() {
+	prometheus.MustRegister(version.NewCollector("lustre_exporter"))
+}
+
+func main() {
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Fprintln(os.Stdout, version.Print("lustre_exporter"))
+		os.Exit(0)
+	}
+
+	log.Infoln("Starting lustre_exporter", version.Info())
+	log.Infoln("Build context", version.BuildContext())
+
+	prometheus.MustRegister(lustre.NewExporter())
+
+	handler := prometheus.Handler()
+
+	http.Handle(*metricsPath, handler)
+	http.HandleFunc("/" func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+			<head><title>Lustre Exporter</title></head>
+			<body>
+			<h1>Lustre Exporter</h1>
+			<p><a href="` + *metricsPath + `">Metrics</a></p>
+			</body>
+			</html>`))
+	})
+
+	log.Infoln("Listening on", *listenAddress)
+	err = http.ListenAndServe(*listenAddress, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+} 
